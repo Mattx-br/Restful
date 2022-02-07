f032543967dce87dd4441b231a9dfafb78b44b76
Anthony Nunes <anthonytx22nunes@gmail.com>
2022-02-07 17:53:39 -0300

    Refactoring code

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,20 +1,43 @@
+let NeDB = require('nedb');
+
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = app => {
 
-    app.get('/users', (req, res) => {
+    let route = app.route('/users');
 
-        res.statusCode = 200; // connection succeed 
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                id: 1,
-                name: 'Batata',
-                email: 'batata@feira.com'
-            }]
+    route.get((req, res) => {
+
+        db.find().sort({ name: 1 }).exec((err, users) => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.statusCode = 200; // connection succeed 
+                res.setHeader('Content-Type', 'application/json');
+                res.json({ users });
+
+            }
+
         });
 
     });
 
-    app.post('/users', (req, res) => {
-        res.json(req.body);
+    route.post((req, res) => {
+
+        db.insert(req.body, (err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+
+                res.status(200).json(user);
+
+            }
+
+        });
     });
 };
