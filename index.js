d59092c2d5caf687e1cad47935e0babca3119d27
Anthony Nunes <anthonytx22nunes@gmail.com>
2022-02-07 16:31:59 -0300

    Adding routes

--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
-    console.log('URL:', req.url);
-    console.log('METHOD:', req.method);
-    console.log("\n");
+let routesIndex = require('./routes/index');
+let routesUsers = require('./routes/users');
+
+let app = express();
+
+app.use(routesIndex);
+app.use('/users', routesUsers);
 
 
-    switch (req.url) {
-        case '/':
 
-            res.statusCode = 200; // connection succeed 
-
-            res.setHeader('Content-Type', 'text/html');
-
-            res.end("<h1>Olá<h1>");
-
-            break;
-
-        case '/users':
-
-            res.statusCode = 200; // connection succeed 
-
-            res.setHeader('Content-Type', 'application/json');
-
-            res.end(JSON.stringify({
-                users: [{
-                    id: 1,
-                    name: 'Batata',
-                    email: 'batata@feira.com'
-                }]
-            }));
-
-            break;
-    }
-});
-
-server.listen(3000, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
     console.log("server on");
 });
