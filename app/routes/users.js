ec628e9b02c105a4d699388d3d6de01014af7b46
Mattx-br <anthonytx22nunes@gmail.com>
2022-02-08 14:17:15 -0300

    Update and Remove

--- app/routes/users.js
+++ app/routes/users.js
@@ -9,6 +9,8 @@ module.exports = app => {
 
     let route = app.route('/users');
 
+    // Getting all registers
+
     route.get((req, res) => {
 
         db.find({}).sort({ name: 1 }).exec((err, users) => {
@@ -25,6 +27,9 @@ module.exports = app => {
         });
 
     });
+
+
+    // Inserting a register 
 
     route.post((req, res) => {
 
@@ -44,6 +49,8 @@ module.exports = app => {
 
     let routeId = app.route('/users/:id');
 
+    // Getting a register by id
+
     routeId.get((req, res) => {
 
         db.findOne({ _id: req.params.id }).exec((err, user) => {
@@ -54,6 +61,34 @@ module.exports = app => {
                 res.status(200).json(user);
             }
 
+        });
+
+    });
+
+    // Updating register
+
+    routeId.put((req, res) => {
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params, req.body));
+            }
+
+        });
+
+    });
+
+    routeId.delete((req, res) => {
+
+        db.remove({ _id: req.params.id }, {}, err => {
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.params));
+            }
         });
 
     });
